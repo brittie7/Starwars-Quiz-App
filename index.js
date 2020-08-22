0e3f57d0f4cfb5d392fa727e0978ab3febdf27f1
Brittany Coleman <brittie7@gmail.com>
2020-08-22 16:40:57 -0400

    updated css

--- index.js
+++ index.js
@@ -1,80 +1,66 @@
-'use strict';
+"use strict";
 
 const STORE = {
-  quizBank: [//1
+  quizBank: [
+    //1
     {
       question: "Who is the most loyal friend and first mate of Han Solo’s? ",
-      options: [
-        "Chewie", 
-        "Jar Jar Binks", 
-        "C3PO", 
-        "R2D2"
-      ],
-      answer: "Chewie"
+      options: ["Chewie", "Jar Jar Binks", "C3PO", "R2D2"],
+      answer: "Chewie",
     },
     //2
     {
       question: "Who is Luke and Leia’s mother? ",
       options: [
         "Princess Amidala",
-        "Princess Tiana", 
+        "Princess Tiana",
         "Princess Palpatine",
-        "Princess Jasmine"
+        "Princess Jasmine",
       ],
-      answer: "Princess Amidala"
+      answer: "Princess Amidala",
     },
     //3
     {
       question: "What bounty hunter captures Han Solo? ",
-      options: [
-        "The Sith", 
-        "Obie Wann", 
-        "Darth Vader", 
-        "Boba Fett"
-      ],
-      answer: "Boba Fett"
+      options: ["The Sith", "Obie Wann", "Darth Vader", "Boba Fett"],
+      answer: "Boba Fett",
     },
     //4
     {
-      question: "Who do Obi-Wan and Yoda give Leia to?", 
+      question: "Who do Obi-Wan and Yoda give Leia to?",
       options: ["Grevious Kahn", "Prad Palto", "Bail Organa", "Count Dooku"],
-      answer: "Bail Organa"
+      answer: "Bail Organa",
     },
     //5
     {
       question: "Who trained Luke Skywalker?",
-      options: [
-        "Anakin Skywalker", 
-        "Yoda", 
-        "Obi-Wan Kenobi", 
-        "Qui-Gon Jinn"
-      ],
-      answer: "Yoda"
-    }
+      options: ["Anakin Skywalker", "Yoda", "Obi-Wan Kenobi", "Qui-Gon Jinn"],
+      answer: "Yoda",
+    },
   ],
 };
 
-let score = 0; 
-let currentQuestionIndex=0;
+let score = 0;
+let currentQuestionIndex = 0;
 let quizLength = STORE.quizBank.length;
 
 function startQuiz() {
   // this function will be responsible for rendering the start quiz page
   // in the DOM
-  console.log('`startQuiz` ran');
-  $('#start').on('click',function(event){
+  console.log("`startQuiz` ran");
+  $("#start").on("click", function (event) {
     $(this).hide();
     displayQuizStatus();
     newQuestion();
     createSubmitButton();
-  })
-};
+  });
+}
 
 //This function will display the current quiz question and answers
 
 function displayQuizStatus() {
-  console.log('`displayQuizStatus` ran');
-  let quizStatusVariable = $('.quizStatus');
+  console.log("`displayQuizStatus` ran");
+  let quizStatusVariable = $(".quizStatus");
   $(`<ul>
   <li class="quizStatusTracker">Question:
       <span class="questionNumber">${currentQuestionIndex}</span>/${quizLength}</li>
@@ -85,139 +71,138 @@ function displayQuizStatus() {
 }
 
 function newQuestion() {
-  console.log('`newQuestion` ran');
+  console.log("`newQuestion` ran");
 
-  $('.questionNumber').text(currentQuestionIndex + 1);
+  $(".questionNumber").text(currentQuestionIndex + 1);
   let currentQuestion = STORE.quizBank[currentQuestionIndex];
-  let currentQuestionPrompt = currentQuestion.question 
-  let currentQuestionOptions = currentQuestion.options
-  let quizSet = $('.questions')
-   quizSet.html(`<div class = questionOnly> ${currentQuestionPrompt}</div>`);
+  let currentQuestionPrompt = currentQuestion.question;
+  let currentQuestionOptions = currentQuestion.options;
+  let quizSet = $(".questions");
+  quizSet.html(`<div class = questionOnly> ${currentQuestionPrompt}</div>`);
 
-  currentQuestionOptions.forEach(function(choice, choiceIndex) {
+  currentQuestionOptions.forEach(function (choice, choiceIndex) {
     $(`<div><label class="answerQuiz" for="${choiceIndex}">
         <input class="radio" type="radio" id="${choiceIndex}" value="${choice}" name="answer" required>
         <span>${choice}</span>
       </label></div>
-      `).appendTo(quizSet)
-  } )
- 
+      `).appendTo(quizSet);
+  });
 }
-function createSubmitButton () {
-  console.log ('`createSubmitButton ran`');
-  let unifiedQuestion = $('#trivia');
-  $(`<input type="submit" id ="checkAnswer" form="trivia" value="Submit">`).appendTo(unifiedQuestion)
+function createSubmitButton() {
+  console.log("`createSubmitButton ran`");
+  let unifiedQuestion = $("#trivia");
+  $(
+    `<input type="submit" id ="checkAnswer" form="trivia" value="Submit">`
+  ).appendTo(unifiedQuestion);
 }
 
 function submitAnswer() {
-  console.log('`submitAnswer ran`');
-  $('#trivia').submit(function(event) {
+  console.log("`submitAnswer ran`");
+  $("#trivia").submit(function (event) {
     event.preventDefault();
-    let userAnswer = $('input[name=answer]:checked', '#trivia').val();
+    let userAnswer = $("input[name=answer]:checked", "#trivia").val();
     let correct = STORE.quizBank[currentQuestionIndex].answer;
-    if (userAnswer===correct) {
-     answerWellYouHave();
+    if (userAnswer === correct) {
+      answerWellYouHave();
       //show happy baby yoda
       //increase score by 1;}
       // hide submit button
       //show next button
     } else {
       tryAgainYouMust();
-        //show disappointed baby yoda
-        //show correct.val()
-        //show next button
+      //show disappointed baby yoda
+      //show correct.val()
+      //show next button
     }
-  })
+  });
 }
 
 function answerWellYouHave() {
-  console.log('`answerWellYouHave ran`')
-  $('#trivia').hide()
-  $('.displayUserStatus').html(
+  console.log("`answerWellYouHave ran`");
+  $("#trivia").hide();
+  $(".displayUserStatus").html(
     `<div class=tempAnswer><img src="./img/babyYoda-RightAnswer.jpeg" alt="Baby Yoda smiling" class="answerChecked">
     <span class="rightAnswer">Answer Correctly You Have</span> <button type="button" id="next">Next</button></div>`
-  )
-  score++
-  $('.score').text(score)
-  
-  $('#next').on('click',function(event){
+  );
+  score++;
+  $(".score").text(score);
 
-    if ((currentQuestionIndex+1)===quizLength) {
+  $("#next").on("click", function (event) {
+    if (currentQuestionIndex + 1 === quizLength) {
       endQuiz();
     } else {
       nextQuestion();
     }
-  })
-  //update the score 
-//$('#trivia')
+  });
+  //update the score
+  //$('#trivia')
 }
 
 function tryAgainYouMust() {
-  console.log('`tryAgainYouMust ran`')
-  $('#trivia').hide()
-  $('.displayUserStatus').html(
+  console.log("`tryAgainYouMust ran`");
+  $("#trivia").hide();
+  $(".displayUserStatus").html(
     `<div class=tempAnswer><img src="./img/babyYoda-WrongAnswer.jpeg" alt="Baby Yoda looking confused" class="answerChecked">
     <p>So certain were you. Next time, closer you must look. The correct answer is:</p>
     <span class="wrongAnswer">${STORE.quizBank[currentQuestionIndex].answer}</span><button type="button" id="next">Next</button></div>`
-  )
-  $('#next').on('click',function(event){
-
-    if ((currentQuestionIndex+1)===quizLength) {
+  );
+  $("#next").on("click", function (event) {
+    if (currentQuestionIndex + 1 === quizLength) {
       endQuiz();
     } else {
       nextQuestion();
     }
-  })
+  });
   //Go to next question function
 }
 
 function nextQuestion() {
-  console.log('`nextQuestion ran`');
-  currentQuestionIndex++
-  $('#trivia').show()
-  $('.displayUserStatus').empty();
+  console.log("`nextQuestion ran`");
+  currentQuestionIndex++;
+  $("#trivia").show();
+  $(".displayUserStatus").empty();
   newQuestion();
   //submitAnswer();
 }
 
-
 function endQuiz() {
-  console.log('`endQuiz ran`');
-  $('.quizStatus').hide();
-  $('.displayUserStatus').empty();
-  $('.displayUserStatus').html(`You got ${score} of ${quizLength} questions right`);
-  $('#trivia').show();
-  $('#trivia').empty();
-  $('#trivia').html(`<button type="button" id="restart">Start New Quiz</button>`);
-  resetQuiz()
+  console.log("`endQuiz ran`");
+  $(".quizStatus").hide();
+  $(".displayUserStatus").empty();
+  $(".displayUserStatus").html(
+    `You got ${score} of ${quizLength} questions right`
+  );
+  $("#trivia").show();
+  $("#trivia").empty();
+  $("#trivia").html(
+    `<button type="button" id="restart">Start New Quiz</button>`
+  );
+  resetQuiz();
 }
 
-function resetQuiz(){
-  console.log('`resetQuiz ran`');
-  $('#restart').on('click',function(event){
+function resetQuiz() {
+  console.log("`resetQuiz ran`");
+  $("#restart").on("click", function (event) {
     score = 0;
     currentQuestionIndex = 0;
-    $('.score').text(score)
+    $(".score").text(score);
     $(this).hide();
-    $('.displayUserStatus').empty();
-    $('#trivia').html(`<div class="questions"></div>`);
-    $('.quizStatus').show();
+    $(".displayUserStatus").empty();
+    $("#trivia").html(`<div class="questions"></div>`);
+    $(".quizStatus").show();
     //displayQuizStatus();
     newQuestion();
     createSubmitButton();
-    
 
-
-    
     //location.reload();
     //restartQuiz();
-  })
+  });
 }
 
 function handleQuiz() {
   startQuiz();
   submitAnswer();
- //hideQuizStatus();
+  //hideQuizStatus();
   //displayQuizStatus();
 }
 // when the page loads, call `handleQuiz`
